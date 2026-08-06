c9b893bd4a3f9ffdd225b28f03ee24b4c6c0c6bd
abmdeve <tundederome@gmail.com>
2026-08-06 02:07:07 +0100

    Nativewind of Tailwind CSS as configured but I don't try with a physical device

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+// import { StyleSheet, Text, View } from "react-native";
+// import "../../global.css"
 
-export default function Index() {
+// export default function Index() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Edit src/app/index.tsx to edit this screen.</Text>
+//       <Text>Nous revenons dans le game après plus d'une année de Shabbat.</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+
+import { Text, View } from "react-native";
+
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-      <Text>Nous revenons dans le game après plus d'une année de Shabbat.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
